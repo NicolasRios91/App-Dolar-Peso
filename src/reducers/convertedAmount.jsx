7d10e2e69c8c00d8bcf5b3b2db027a61ed216a67
unknown <nicolasrios91@hotmail.com>
2021-07-13 18:51:52 -0300

    added $ symbol

--- src/reducers/convertedAmount.jsx
+++ src/reducers/convertedAmount.jsx
@@ -1,13 +1,17 @@
 const convertedAmountReducer = (state = 0, action) => {
   switch (action.type) {
     case "DOLLAR_TO_PESO":
-      return action.payload.amount * action.payload.dollarValue;
+      return (
+        (action.payload.amount * action.payload.dollarValue).toFixed(2) + " $"
+      );
     case "PESO_TO_DOLLAR":
-      return action.payload.amount / action.payload.dollarValue;
+      return (
+        (action.payload.amount / action.payload.dollarValue).toFixed(2) + " $"
+      );
     case "DEFAULT":
-      return action.payload.amount;
+      return action.payload.amount + " $";
     default:
-      return state;
+      return state + " $";
   }
 };
 
